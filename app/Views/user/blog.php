73b48d85e6d98b6ad1e1baa494a3bb0372b719be
alendro25 <57282213+alendro25@users.noreply.github.com>
2021-04-19 13:53:33 +0700

    second commit

--- app/Views/user/blog.php
+++ app/Views/user/blog.php
@@ -22,11 +22,11 @@
 
 <body>
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container">
+  <nav id="menu-utama" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <div class="container px-lg-5">
       <div class="brand">
         <img src="/img/Logo Apps Twins Robo.png" alt="" />
-        <a class="navbar-brand" href="#">Twins Robo Apps</a>
+        <a class="navbar-brand" href="#">Twins Robo App</a>
       </div>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -533,8 +533,11 @@
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+  <script src="/js/script.js"></script>
+
 
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// JS untuk Navbar ketika di scroll hilang
+// (function() {
+//     var documentElem = $(document),
+//     nav = $('nav'),
+//     lastScrollTop = 0;
+
+//     documentElem.on('scroll', function() {
+//     var currentScrollTop = $(this).scrollTop();
+
+//     // scroll down
+//     if(currentScrollTop > lastScrollTop) nav.addClass('hidden');
+//     // scrollTop
+//     else nav.removeClass('hidden');
+
+//     lastScrollTop = currentScrollTop;
+//     });
+
+//     })();
